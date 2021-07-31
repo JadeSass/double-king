46e94f3b4b8baffa60fdd9957124ee7b07d23880
Jade Sass <pelumiezekiel.developer@gmail.com>
2021-07-31 10:48:56 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#388E3C">
+        <meta content="Get Exclusive Access To Premium Informations That Will Make You Rich Within a Short Period Of Time Using Just Your Phone Or Laptop" name="description">
+        <title>How To Earn 6-7 Figures a Month By Pressing Your Phone Or Laptop</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="{{ asset('css/font-awesome/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <div class="bg-particles black darken-3" id="particles-js">
+                <div class="container">
+                    <div class="panel" style="padding:10px;">
+                        <h4 class="black-text center-align"><b>HOW TO EARN <br>
+                        <span class="red-text">6 FIGURES</span> <br>IN A MONTH BY PRESSING YOUR PHONE OR LAPTOP.</b></h4>
+                        <h5 class="green-text center-align"><b>DISCOVER THE SECRET OF MAKING <span class="red-text">6 FIGURES</span> ONLINE.</b></h5><br>
+                        <div class="container">
+                        <h6 style="line-height: 35px;">Many people like the idea of earning online but don't know how to go about it. The reason is not hidden, it is because they lack the skills required to earn online and if this is you, then don't worry, this is where your journey to earning real-time income online begins.</h6><br>
+                        <div class="row center-align">
+                                <div class="col s12 m12 l12 xl12 center-align">
+                                    <img src="{{ url('img/FB_IMG_1623313489159.jpg') }}" class="responsive-img materialboxed center-align" alt="proof"><br>
+                                </div><br></div>
+                        <h6 style="line-height: 35px;">My name is <b>Israel Adeleke Adesanya</b> and I am the king of this domain. I help people to build sustainable online businesses in different industries such as e-commerce, affiliate marketing, and other business industries capable of making them 6 figures monthly.</h6><br>
+
+                        <h6 style="line-height: 35px;">This <b>FREE</b> video I'm about to send to you now explains how you can set up a profitable online business that can be generating you up to 750K a month in the affiliate marketing industry.</h6>
+                        </div>
+                        <div class="access center-align">
+                            <a href="https://wa.link/k42c0k" class="btn btn-flat btn-large green darken-3 white-text"><b>Send the video</b></a> <br> <br>
+                            <h5><b>BELOW ARE SOME TESTIMONIES OF MY STUDENTS THAT STARTED MAKING <b class="red-text">6-7 FIGURES MONTHLY</b> AFTER THEY WATCHED THE SAME VIDEO I'M ABOUT TO SEND YOU NOW.</b></h5> <br> <br>
+                        </div>
+                        <div class="image-testimonies containe center-align">
+                            <div class="row center-align">
+                                <div class="col s12 m12 l12 xl12 center-align">
+                                    <img src="{{ url('img/proof-1.png') }}" class="responsive-img materialboxed center-align" alt="proof"><br>
+                                </div><br>
+                                <div class="col s12 m12 l12 xl12 center-align">
+                                    <img src="{{ url('img/proof-2.png') }}" class="responsive-img materialboxed center-align" alt="proof">
+                                </div><br>
+                                <div class="col s12 m12 l12 xl12 center-align">
+                                    <img src="{{ url('img/proof-3.png') }}" class="responsive-img materialboxed center-align" alt="proof">
+                                </div>
+                                <p class="red-text center-align">Don't miss out.</p>
+                                <a href="https://wa.link/k42c0k" class="btn btn-flat btn-large green darken-3 white-text"><b>Send the video</b></a><br><br>
+                                <h5 class="center-align"> <b>Disclaimer</b> <br> The results in this training are not typical and will require implementation. It is also not in any way part of Facebook and Alphabet Inc.</h5>
+                            </div>
+                        </div>
+                        <p class="grey-text center-align"><b>&copy; {{ date('Y') }} Israel Adeleke Adesanya.</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="{{ asset('js/particles.min.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
